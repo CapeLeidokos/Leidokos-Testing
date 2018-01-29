1b1bd5dcd0581b7b417905d654571e735cb7a465
Florian Fleissner <fleissner@inpartik.de>
2018-01-29 18:05:57 +0100

    Fixes for MacOS

--- travis/test_kaleidoscope_module.sh
+++ travis/test_kaleidoscope_module.sh
@@ -56,7 +56,7 @@ fi
 
 if [ "${TRAVIS_OS_NAME}" == "osx" ]; then
    ls -l /usr/local/lib
-   python_so_path="/usr/local/lib/libboost_python.dylib"
+   python_so_path="/usr/local/lib/libboost_python3.dylib"
    python_executable="/usr/local/bin/python3"
 else
    python_so_path="/usr/lib/x86_64-linux-gnu/libboost_python-py34.so"
