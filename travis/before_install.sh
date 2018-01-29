aefb4628d4a6796f7c5ed651063986b829e9dd80
Florian Fleissner <fleissner@inpartik.de>
2018-01-29 17:54:53 +0100

    Fixes for MacOS

--- travis/before_install.sh
+++ travis/before_install.sh
@@ -25,7 +25,7 @@ if [ "${TRAVIS_OS_NAME}" == "osx" ]; then
    brew install python3
 #    brew install boost --with-python
    brew info boost-python
-   brew install boost-python
+   brew install boost-python --with-python3 --without-python
 #    sudo easy_install pip3
    sudo -H pip3 install pyyaml
    sudo -H pip3 install sphinx
