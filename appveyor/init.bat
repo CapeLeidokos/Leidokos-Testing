e6a24e2761ca4c4cc4ae4c41accbc15d604c7dda
Florian Fleissner <fleissner@inpartik.de>
2018-02-06 19:20:38 +0100

    Appveyor

--- appveyor/init.bat
+++ appveyor/init.bat
@@ -6,6 +6,8 @@ C:\msys64\usr\bin\pacman --noconfirm -S mingw-w64-x86_64-python3-pip
 C:\msys64\usr\bin\pacman --noconfirm -S mingw-w64-x86_64-boost
 C:\msys64\usr\bin\pacman --noconfirm -S mingw-w64-x86_64-ninja
 
+C:\msys64\usr\bin\pacman -Ql mingw-w64-x86_64-ninja
+
 C:\msys64\mingw64\bin\python3.exe "C:\msys64\mingw64\bin\pip3-script.py" install pyyaml
 
 rem sphinx is only required to build the Leidokos-Python's API
--- appveyor/test_kaleidoscope_module.bat
+++ appveyor/test_kaleidoscope_module.bat
@@ -35,6 +35,8 @@ set python_executable=C:\msys64\mingw64\bin\python3.exe
 set cmake_executable=C:\msys64\mingw64\bin\cmake.exe
 set ctest_executable=C:\msys64\mingw64\bin\ctest.exe
 
+C:\msys64\mingw64\bin\ninja.exe --version
+
 %cmake_executable% ^
    -G Ninja ^
    "-DLEIDOKOS_TESTING_TARGET_URL=%module_git_url%" ^
