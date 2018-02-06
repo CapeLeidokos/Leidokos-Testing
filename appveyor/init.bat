70d9e6bbcdb2d368bc1c1a1e141be8f7831b0865
Florian Fleissner <fleissner@inpartik.de>
2018-02-06 16:51:45 +0100

    Appveyor

--- appveyor/init.bat
+++ appveyor/init.bat
@@ -1,2 +1,2 @@
-C:\msys64\usr\bin\pacman -Syu mingw-w64-x86_64-python3
-C:\msys64\usr\bin\pacman -Syu mingw-w64-x86_64-boost
+C:\msys64\usr\bin\pacman --noconfirm -S mingw-w64-x86_64-python3
+C:\msys64\usr\bin\pacman --noconfirm -S mingw-w64-x86_64-boost
