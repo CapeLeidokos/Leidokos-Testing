db6dcab458831660913425cca130cd77964cb2ee
Florian Fleissner <fleissner@inpartik.de>
2018-02-06 16:47:07 +0100

    Appveyor

--- appveyor/init.bat
+++ appveyor/init.bat
@@ -1,2 +1,2 @@
-C:\msys64\usr\bin\pacman --sync mingw-w64-x86_64-python3
-C:\msys64\usr\bin\pacman --sync mingw-w64-x86_64-boost
+C:\msys64\usr\bin\pacman --force -S mingw-w64-x86_64-python3
+C:\msys64\usr\bin\pacman --force -S mingw-w64-x86_64-boost
